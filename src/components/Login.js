6df7f6b33729244bbd02663e5a8ad9a791d5b818
VenkatChalamallaaaaaa <129875373+VenkatChalamallaaaaaa@users.noreply.github.com>
2023-11-23 19:42:33 +0530

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ function Login(){
 
   const handleSubmit=(e)=>{
     e.preventDefault()
-    axios.post('http://localhost:3001/login',{email,password})
+    axios.post('https://lms-server1.onrender.com/login',{email,password})
     .then(result => {
       console.log(result)
       if(result.data ==='Success'){
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,7 +11,7 @@ function Signup(){
   const navigate = useNavigate()
   const handleSubmit=(e)=>{
     e.preventDefault()
-    axios.post('http://localhost:3001/register',{name,email,password})
+    axios.post('https://lms-server1.onrender.com/register',{name,email,password})
     .then(result => {console.log(result)
     navigate('/login')
     })
